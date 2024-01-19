a870ebcf3ac1f8d26e7f51bc690030aa47cd837e
deuzleor <du_lo@hotmail.com>
2024-01-18 23:08:23 -0600

    arreglo de autoload

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -46,8 +46,6 @@ class Autoload extends AutoloadConfig
     public $psr4 = [
         APP_NAMESPACE => APPPATH, // For custom app namespace
         'Config'      => APPPATH . 'Config',
-        'App'         => APPPATH,
-        'CodeIgniter' => SYSTEMPATH . 'CodeIgniter',
         'RESTful'     => APPPATH . 'ThirdParty/RESTful',
     ];
 
